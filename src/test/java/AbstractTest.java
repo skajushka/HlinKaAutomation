83f5f957a4fd29a125e8f57f45762492b0f2742b
Kaciaryna <kaciaryna@primvs.antonivs.albvs>
2018-05-29 22:24:10 +0300

    Added cross-platform driver selection

--- src/test/java/AbstractTest.java
+++ src/test/java/AbstractTest.java
@@ -4,15 +4,26 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public abstract class AbstractTest {
 
     protected static WebDriver webDriver;
 
+    @BeforeClass
+    public static void setupClass() {
+        String osName = System.getProperty("os.name");
+
+        if (osName.startsWith("Windows")) {
+            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        } else {
+            System.setProperty("webdriver.chrome.driver", "chromedriver");
+        }
+    }
+
     @BeforeMethod
     public void openWebsite() {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/SidorenkoEV/IdeaProjects/HlinKaAutomation/chromedriver.exe");
         webDriver = new ChromeDriver();
         StartPage startPage = new StartPage(webDriver);
         webDriver.get(startPage.getLinkURL());
